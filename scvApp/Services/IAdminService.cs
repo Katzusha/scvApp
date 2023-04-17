c5b5125efaef47929a2b11d761fe3ea331e41c85
Katzsha <kosak.andraz@gmail.com>
2023-04-17 19:00:50 +0200

    Edit admins

--- scvApp/Services/IAdminService.cs
+++ scvApp/Services/IAdminService.cs
@@ -9,6 +9,10 @@ namespace scvApp.Services
         Task AddNewAdmin(Admin admin);
 
         Task<List<Admin>> GetAllAdmins();
+
+        Task UpdateAdmin(Admin admin);
+
+        Task DeleteAdmin(Admin admin);
     }
 
     public class AdminService : IAdminService
@@ -27,6 +31,28 @@ namespace scvApp.Services
             using (var context = new AppDbContext())
             {
                 return await context.Admins.ToListAsync();
+            }
+        }
+
+        public async Task UpdateAdmin(Admin admin)
+        {
+            using (var context = new AppDbContext())
+            {
+                context.Admins.Update(admin);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteAdmin(Admin admin)
+        {
+            using (var context = new AppDbContext())
+            {
+                var oldAdmin = await context.Admins.FindAsync(admin.Id);
+                if (oldAdmin != null)
+                {
+                    context.Admins.Remove(oldAdmin);
+                    await context.SaveChangesAsync();
+                }
             }
         }
     }
