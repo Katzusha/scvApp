8625d44a686910c0b468ff967c5b11ed798ce857
Katzusha <kosak.andraz@gmail.com>
2023-04-18 10:58:07 +0200

    a

--- scvApp/Services/IDateService.cs
+++ scvApp/Services/IDateService.cs
@@ -9,6 +9,10 @@ namespace scvApp.Services
         Task AddNewDate(Date date);
 
         Task<List<Date>> GetAllDates();
+
+        Task UpdateDate(Date date);
+
+        Task DeleteDate(Date date);
     }
 
     public class DateService : IDateService
@@ -27,6 +31,28 @@ namespace scvApp.Services
             using (var context = new AppDbContext())
             {
                 return await context.Dates.ToListAsync();
+            }
+        }
+
+        public async Task UpdateDate(Date date)
+        {
+            using (var context = new AppDbContext())
+            {
+                context.Dates.Update(date);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteDate(Date date)
+        {
+            using (var context = new AppDbContext())
+            {
+                var oldDate = await context.Dates.FindAsync(date.Id);
+                if (oldDate != null)
+                {
+                    context.Dates.Remove(oldDate);
+                    await context.SaveChangesAsync();
+                }
             }
         }
     }
