408f96ba62959452380568f3e8f9f4d03a6466e1
Katzsha <kosak.andraz@gmail.com>
2023-05-02 14:12:09 +0200

    sub admin panels complete

--- scvApp/Services/ISchoolSubmitService.cs
+++ scvApp/Services/ISchoolSubmitService.cs
@@ -1,4 +1,5 @@
-﻿using scvApp.dbcontext;
+﻿using Microsoft.EntityFrameworkCore;
+using scvApp.dbcontext;
 using scvApp.Models;
 
 namespace scvApp.Services
@@ -6,6 +7,12 @@ namespace scvApp.Services
     public interface ISchoolSubmitService
     {
         Task AddNewSchoolSubmit(SchoolSubmit schoolsubmit);
+
+        Task<List<SchoolSubmit>> GetAllSchoolSubmits();
+
+        Task UpdateSchoolSubmit(SchoolSubmit schoolsubmit);
+
+        Task DeleteSchoolSubmit(SchoolSubmit schoolsubmit);
     }
 
     public class SchoolSubmitService : ISchoolSubmitService
@@ -16,6 +23,36 @@ namespace scvApp.Services
             {
                 await context.SchoolSubmits.AddAsync(schoolsubmit);
                 await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<SchoolSubmit>> GetAllSchoolSubmits()
+        {
+            using (var context = new AppDbContext())
+            {
+                return await context.SchoolSubmits.ToListAsync();
+            }
+        }
+
+        public async Task UpdateSchoolSubmit(SchoolSubmit schoolsubmit)
+        {
+            using (var context = new AppDbContext())
+            {
+                context.SchoolSubmits.Update(schoolsubmit);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteSchoolSubmit(SchoolSubmit schoolsubmit)
+        {
+            using (var context = new AppDbContext())
+            {
+                var oldschoolsubmit = await context.SchoolSubmits.FindAsync(schoolsubmit.Id);
+                if (oldschoolsubmit != null)
+                {
+                    context.SchoolSubmits.Remove(oldschoolsubmit);
+                    await context.SaveChangesAsync();
+                }
             }
         }
     }
